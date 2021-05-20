2f62b60af2dcbbe23f48bb4b9564074ec368d0a3
Mikhail <stroryz@gmail.com>
2021-05-20 15:53:48 +0300

    file modified

--- script/smooth-scroll.js
+++ script/smooth-scroll.js
@@ -1,12 +1,56 @@
-const smoothScrollElems = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
+// const smoothScrollElems = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
 
-smoothScrollElems.forEach(link => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault();
+// Скролл через behavior (еще слабая поддержка браузерами)
+// smoothScrollElems.forEach(link => {
+//   link.addEventListener('click', (event) => {
+//     event.preventDefault();
 
-    const id = link.getAttribute('href').substring(1);
-    document.getElementById(id).scrollIntoView({
-      behavior: 'smooth',
-    });
-  })
-})
+//     const id = link.getAttribute('href').substring(1);
+//     document.getElementById(id).scrollIntoView({
+//       behavior: 'smooth',
+//     });
+//   })
+// })
+
+const SPEED = 0.3;
+
+const scroll = (event) => {
+  event.preventDefault();
+
+  const target = event.target;
+  if(target.matches('[href^="#"]')) {
+    const pageY = window.pageYOffset;
+    // const hash = target.href.replace(/[^#]*(.*)/, '$1');
+    const hash = target.getAttribute('href');
+    if (hash === "#") return;
+    const elem = document.querySelector(hash);
+    const elemCoordinata = elem.getBoundingClientRect().top;
+
+    
+    
+    let start = 0;
+    const step = time => {
+      if (!start) {
+        start = time;
+      }
+      const progress = time - start;
+
+      const r = (elemCoordinata < 0 ? 
+          Math.max(pageY - progress / SPEED, pageY + elemCoordinata) : 
+          Math.min(pageY + progress / SPEED, pageY + elemCoordinata));
+      
+      window.scrollTo(0, r);
+
+      if (r < pageY + elemCoordinata) {
+        requestAnimationFrame(step);
+      }
+    }
+
+    requestAnimationFrame(step);
+
+
+  }
+
+}
+
+document.body.addEventListener('click', scroll);
